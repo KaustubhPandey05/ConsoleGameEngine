26c012fe0e7934af45772c71e762e9cbcfd3a044
KaustubhPandey05 <pandeykaustubh96@gmail.com>
2025-03-23 00:40:25 +0530

    commiting new iteration on cube

--- ConsoleGameEngine/ConsoleGameEngine.cpp
+++ ConsoleGameEngine/ConsoleGameEngine.cpp
@@ -9,6 +9,7 @@ struct vec3D
 struct triangle
 {
     vec3D p[3];
+    olc::Pixel col;//this should be stored per vertex instead
 };
 struct mesh
 {
@@ -29,6 +30,8 @@ public:
 private:
     mesh meshCube;
     mat4x4 matProj;
+    vec3D vCamera;
+
     float fTheta = 0.0f;
     void MultiplyMatrix(vec3D& i, vec3D& output, mat4x4& m)
     {
@@ -36,7 +39,7 @@ private:
         output.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
         output.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
         float w =  i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
-        if (w != 0.0f)
+        if (w != -0.0f)
         {
             output.x /= w;
             output.y /= w;
@@ -44,6 +47,13 @@ private:
         }
     }
 public:
+    olc::Pixel GetColour(float lum)
+    {
+        using namespace olc;
+        int i = (int)(lum * 255.0f);
+        i = std::max(0, std::min(255, i));
+        return Pixel( i,i,i );
+    }
     bool OnUserCreate()override
     {
         meshCube.tris = {
@@ -75,13 +85,13 @@ public:
         //populate the perspective matrix
         float fNear = 0.1f;
         float fFar = 1000.0f;
-        float fFov = 90.0f;
+        float fFov = 80.0f;
         float fAspect = (float)ScreenHeight() / (float)ScreenWidth();
         float fFovRad = 1.0 / tanf(fFov * 0.5f / 180.0f * 3.14159265358979323846f);//TODO: (fFov * 0.5f/ 180.0f * 3.14159f);
         matProj.m[0][0] = fAspect * fFovRad;
         matProj.m[1][1] = fFovRad;
         matProj.m[2][2] = fFar / (fFar - fNear);
-        matProj.m[3][2] = (- fFar * fNear )/ (fFar - fNear);
+        matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
         matProj.m[2][3] = 1.0f;
         matProj.m[3][3] = 0.0f;
         return true;
@@ -132,24 +142,68 @@ public:
             triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
             triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
             triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
-            MultiplyMatrix(triTranslated.p[0], triProjected.p[0], matProj);
-            MultiplyMatrix(triTranslated.p[1], triProjected.p[1], matProj);
-            MultiplyMatrix(triTranslated.p[2], triProjected.p[2], matProj);
-            //scalling the triangle
-            triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
-            triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
-            triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
 
-            triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
-            triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
-            triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
-            triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
-            triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
-            triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+            vec3D normal, line1, line2;
+            line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
+            line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
+            line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
 
-            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
-                triProjected.p[1].x, triProjected.p[1].y,
-                triProjected.p[2].x, triProjected.p[2].y,olc::RED);
+            line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
+            line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
+            line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
+
+            normal.x = line1.y * line2.z - line1.z * line2.y;
+            normal.y = line1.z * line2.x - line1.x * line2.z;
+            normal.z = line1.x * line2.y - line1.y * line2.x;
+
+            float l = sqrtf(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+
+            normal.x /= l;
+            normal.y /= l;
+            normal.z /= l;
+            //if (normal.z < 0)
+            if(normal.x * (triTranslated.p[0].x - vCamera.x )+
+               normal.y * (triTranslated.p[0].y - vCamera.y ) +
+               normal.z * (triTranslated.p[0].z - vCamera.z ) < 0.0f)
+            {
+
+                //Illumination
+                vec3D light_direction = { 0.0f,0.0f,-1.0f };
+                //normalise the light vector
+                float lsqr = sqrtf(light_direction.x * light_direction.x + light_direction.y * light_direction.y + light_direction.z * light_direction.z);
+                light_direction.x /= lsqr;
+                light_direction.y /= lsqr;
+                light_direction.z /= lsqr;
+                float dp = normal.x * light_direction.x + normal.y * light_direction.y + normal.z * light_direction.z;//dot product with the light vector so we can check wheter they are in same direction
+                
+                olc::Pixel c = GetColour(dp);//get the gray shade by looking at the dot product value
+                triTranslated.col = c;//this is very console specific stuff
+
+                MultiplyMatrix(triTranslated.p[0], triProjected.p[0], matProj);
+                MultiplyMatrix(triTranslated.p[1], triProjected.p[1], matProj);
+                MultiplyMatrix(triTranslated.p[2], triProjected.p[2], matProj);
+                triProjected.col = triTranslated.col;
+
+                //scalling the triangle
+                triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+                triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+                triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+
+                triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+                triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+                triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+                triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+                triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+                triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+                //SetPixelMode(olc::Pixel::Mode::ALPHA);
+                FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                    triProjected.p[1].x, triProjected.p[1].y,
+                    triProjected.p[2].x, triProjected.p[2].y, triProjected.col);
+
+                DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                    triProjected.p[1].x, triProjected.p[1].y,
+                    triProjected.p[2].x, triProjected.p[2].y,olc::BLACK);
+            }
         }
         return true;
     }
@@ -159,16 +213,4 @@ int main()
     Engine3D demo;
     if (demo.Construct(256, 240, 4, 4))
         demo.Start();
-    std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
